9c239e2d8c86dd7b54d650f4dfdcfc4e17ba270e
Flipper95 <Flipper95@users.noreply.github.com>
2018-09-30 13:56:28 +0300

    Admin area
    Added admin area with view users, adding and deleting users from role, users searching

--- EnglishLearning/App_Start/BundleConfig.cs
+++ EnglishLearning/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace EnglishLearning
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapAdditional").Include(
+                "~/Scripts/bootstrap.bundle*"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
---
+++ EnglishLearning/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,113 @@
+﻿using EnglishLearning.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+//using Microsoft.AspNet.Identity;
+//using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnglishLearning.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class UsersController : Controller
+    {
+
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        public UsersController() {
+            ViewBag.Roles = GetRoles().ToList();
+        }
+
+        // GET: Admin/Users
+        public ActionResult Index()
+        {
+            //IList<string> roles = new List<string>();
+            //ApplicationUserManager userManager = HttpContext.GetOwinContext()
+            //                                .GetUserManager<ApplicationUserManager>();
+            //var context = new ApplicationDbContext();
+
+            var query = from users in context.Users
+                        select users;
+            //ApplicationUser user = userManager.FindByEmail(User.Identity.Name);
+            //if (user != null)
+            //    roles = userManager.GetRoles(user.Id);
+            //ViewBag.Roles = GetRoles().ToList();
+            return View(query);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string SearchEmail) {
+            if (string.IsNullOrEmpty(SearchEmail))
+            {
+                return Index();
+            }
+            else
+            {
+                var query = from users in context.Users
+                            where users.Email.Contains(SearchEmail)
+                            select users;
+                //ViewBag.Roles = GetRoles().ToList();
+                return View(query);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult UserRole(string RoleEmail, string submit) {
+            ViewBag.UserRoleModified = true;
+            ViewBag.UserModified = RoleEmail;
+            ApplicationUserManager userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+            //var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var user = from users in context.Users
+                       where users.Email == RoleEmail
+                       select users;
+            if (submit == "add") {
+                userManager.AddToRole(user.First().Id, Request.Form["UserRoleEdit"].ToString());
+            }
+            if (submit == "delete") {
+                userManager.RemoveFromRole(user.First().Id, Request.Form["UserRoleEdit"].ToString());
+            }
+            var query = from users in context.Users
+                        select users;
+            return View("Index", query);
+        }
+
+        //В данному функціоналі немає сенсу, нові ролі не впливають на доступ (можливо застосовувати IAuthorizationFilter)
+        //[HttpPost]
+        //public ActionResult RoleEdit(string Role, string submit)
+        //{
+        //    ViewBag.RoleModified = true;
+        //    var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+        //    if (submit == "add")
+        //    {
+        //        IdentityRole newRole = new IdentityRole(Role);
+        //        if (!roleManager.RoleExists(Role))
+        //        {
+        //            roleManager.Create(newRole);
+        //        }
+        //    }
+        //    if (submit == "delete")
+        //    {
+        //        var query = from roles in context.Roles
+        //                    where roles.Name == Role
+        //                    select roles;
+        //        if (roleManager.RoleExists(Role))
+        //        {
+        //            roleManager.Delete(query.First());
+        //        }
+        //    }
+        //    var query1 = from users in context.Users
+        //                 select users;
+        //    return View("Index", query1);
+        //}
+
+        private IQueryable<IdentityRole> GetRoles() {
+            var query1 = from roles in context.Roles
+                         select roles;
+            return query1;
+        }
+    }
+}
